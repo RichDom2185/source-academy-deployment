2a7370bab6b3f15745984bad128170971bce62ef
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-15 18:20:34 +0800

    Fix backend entrypoint starting prematurely

--- backend-entrypoint.sh
+++ backend-entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+while ! pg_isready -q -h "$PG_HOST" -U "$PG_USER"; do
+    echo "$(date) - waiting for database to start"
+    sleep 0.5
+done
+
 # Create, migrate, and seed database if it doesn't exist.
 if [[ -z `psql -h "$PG_HOST" -U "$PG_USER" -lt | cut -d '|' -f 1 | grep -w "$PG_DATABASE"` ]]; then
     echo "Database $PG_DATABASE does not exist. Creating..."
